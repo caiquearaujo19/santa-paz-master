44a5b2bc72eec6e0a44b179bb4b216a5b75b9f7a
Caique Araujo <cam@cesar.org.br>
2022-04-30 16:04:44 -0300

    Removendo o ano 2021 da lista de anos

--- src/AppStore.js
+++ src/AppStore.js
@@ -2,6 +2,6 @@ import create from 'zustand';
 
 export const useAppStore = create(set => ({
   year: new Date().getFullYear().toString(),
-  yearList: ["2021", "2022"],
+  yearList: ["2022"],
   changeYear: (year) => set(() => ({ year: year }))
 }));
